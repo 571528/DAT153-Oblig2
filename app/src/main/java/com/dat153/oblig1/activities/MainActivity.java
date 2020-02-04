518871fc45c209944d1984cda85209ffc849665d
571528 <571528@stud.hvl.no>
2020-02-04 10:10:15 +0100

    main activity: quiz and db buttons visibility set

--- app/src/main/java/com/dat153/oblig1/activities/MainActivity.java
+++ app/src/main/java/com/dat153/oblig1/activities/MainActivity.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.dat153.oblig1.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     EditText insertName;
     Button saveOwnerBtn;
     SharedPreferences sp;
+    ImageButton dbButton;
+    ImageButton quizButton;
     public static final String WELCOME_PREF = "welcomePref";
 
 
@@ -27,35 +32,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Define preferences and views
         sp = getSharedPreferences(WELCOME_PREF, Context.MODE_PRIVATE);
         welcomeTxt = findViewById(R.id.welcomeText);
         insertName = findViewById(R.id.ownerText);
         saveOwnerBtn = findViewById(R.id.saveOwnerBtn);
+        dbButton = findViewById(R.id.dbButton);
+        quizButton = findViewById(R.id.quizButton);
+        //Make database and quiz button invisible unless there already is an owner
+        dbButton.setVisibility(View.INVISIBLE);
+        quizButton.setVisibility(View.INVISIBLE);
         if (!sp.getAll().isEmpty()) {
+            // If there is an owner make only welcome message AND quiz and db buttons visible
             insertName.setVisibility(View.INVISIBLE);
             saveOwnerBtn.setVisibility(View.INVISIBLE);
             welcomeTxt.setText("Welcome back " + sp.getAll().get("owner").toString());
+            dbButton.setVisibility(View.VISIBLE);
+            quizButton.setVisibility(View.VISIBLE);
+
+
         }
 
     }
 
-    // Save owners name to database
+    // Save owners name to shared preferences
     public void saveOwner(View view) {
         String n = insertName.getText().toString();
         SharedPreferences.Editor editor = sp.edit();
         editor.putString("owner", n);
         editor.commit();
+        // Make only welcome message visible
         insertName.setVisibility(View.INVISIBLE);
         saveOwnerBtn.setVisibility(View.INVISIBLE);
+        dbButton.setVisibility(View.VISIBLE);
+        quizButton.setVisibility(View.VISIBLE);
         welcomeTxt.setText("Welcome back " + sp.getAll().get("owner").toString());
+
     }
 
-
+    // To database activity
     public void toDatabase(View view) {
         Intent intent = new Intent(this, DatabaseActivity.class);
         startActivity(intent);
     }
 
+    // To quiz activity
     public void toQuiz(View view) {
         Intent intent = new Intent(this, QuizActivity.class);
         startActivity(intent);
